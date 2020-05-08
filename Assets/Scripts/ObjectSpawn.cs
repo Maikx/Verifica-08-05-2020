29f9ac2ebcf193e37ca12f46a929f95d6648e4c8
Maikx <mikeybee2000@gmail.com>
2020-05-08 11:21:36 +0200

    Map Change, spawn Food

---
+++ Assets/Scripts/ObjectSpawn.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+using Quaternion = UnityEngine.Quaternion;
+
+public class ObjectSpawn : MonoBehaviour
+{
+    public GameObject Foodpreab;
+
+    public Vector3 center;
+    public Vector3 size;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnFood();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Q))
+        {
+            SpawnFood();
+        }
+    }
+
+    public void SpawnFood()
+    {
+        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x /2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
+
+        Instantiate(Foodpreab, pos, Quaternion.Euler(260, 180, 0));
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1,0,0,0.5f);
+        Gizmos.DrawCube(center, size);
+    }
+}
